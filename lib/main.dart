bf192ba65b78ea1428791eb81d31946090be8f21
Rostislav <shandov@i.ua>
2021-01-05 23:10:07 +0200

    Create first project

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Testing')),
+      body: Container(
+        child: Center(
+          child: Text("Smth text"),
+        ),
+      ),
+    );
+  }
+}
